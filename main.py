3d7d325d26ec4834285d19cd18814e5034977e40
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-23 01:17:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from aiogram import executor
+from loader import dp
+import handlers
 
+from db.database import engine
+from db.models import Base
+
+async def on_startup(dp):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, on_startup=on_startup)
