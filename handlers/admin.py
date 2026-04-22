46b2c954626c2d1b060147c31110a17f1b3a869d
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-23 01:16:08 +0300

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,1 +1,9 @@
+from aiogram import types
+from loader import dp
+from config import ADMINS
 
+@dp.message_handler(commands=["admin"])
+async def admin(message: types.Message):
+    if message.from_user.id not in ADMINS:
+        return
+    await message.answer("Админка")
