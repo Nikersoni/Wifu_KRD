41b649d05bccbc2961e74b9419e1618df62d9138
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-23 01:13:30 +0300

    Update top_cache.py

--- utils/top_cache.py
+++ utils/top_cache.py
@@ -1,1 +1,10 @@
+import time
 
+CACHE = {"data": None, "time": 0}
+
+def expired():
+    return time.time() - CACHE["time"] > 21600
+
+def set_cache(data):
+    CACHE["data"] = data
+    CACHE["time"] = time.time()
