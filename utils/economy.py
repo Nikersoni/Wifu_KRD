fcef3c14eac034d4a6fee8cf829e66e2a17737c4
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-23 01:12:42 +0300

    Update economy.py

--- utils/economy.py
+++ utils/economy.py
@@ -1,1 +1,13 @@
+import time
 
+DAILY_LIMIT = 500
+
+def can_earn(user):
+    now = int(time.time())
+    if now - user.last_reset > 86400:
+        user.daily_earned = 0
+        user.last_reset = now
+    return user.daily_earned < DAILY_LIMIT
+
+def add_earn(user, amount):
+    user.daily_earned += amount
