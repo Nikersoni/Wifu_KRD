aba28ac820e6249d2437032f1c1eaa7058c0656b
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-23 01:11:45 +0300

    Update users.py

--- db/queries/users.py
+++ db/queries/users.py
@@ -1,1 +1,13 @@
+from sqlalchemy import select
+from db.models import User
 
+async def get_or_create_user(db, user_id):
+    result = await db.execute(select(User).where(User.id == user_id))
+    user = result.scalar()
+
+    if not user:
+        user = User(id=user_id)
+        db.add(user)
+        await db.commit()
+
+    return user
