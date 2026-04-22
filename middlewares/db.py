db09e093141ab5ec4c7ab4834d39e8a6036447fc
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-23 02:01:31 +0300

    Update db.py

--- middlewares/db.py
+++ middlewares/db.py
@@ -3,11 +3,14 @@ from db.database import async_session
 
 
 class DBMiddleware(BaseMiddleware):
+
     async def on_pre_process_message(self, message, data):
         session = async_session()
         data["db"] = session
 
     async def on_post_process_message(self, message, data, exception):
-        session = data.get("db")
-        if session:
-            await session.close()
+        # ✅ проверка что data — это dict
+        if isinstance(data, dict):
+            session = data.get("db")
+            if session:
+                await session.close()
