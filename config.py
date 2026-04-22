33e890464c37f022d2f7693ab9717b80087d9dd0
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-23 02:19:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,20 +3,5 @@ import os
 TOKEN = os.getenv("BOT_TOKEN")
 DB_URL = os.getenv("DATABASE_URL")
 
-ADMINS = [8200958216]
-
-
-# ⚠️ Проверки (очень важно)
-if not TOKEN:
-    raise ValueError("❌ BOT_TOKEN не задан")
-
-if not DB_URL:
-    raise ValueError("❌ DATABASE_URL не задан")
-
-
-# 🔥 Фикс для Railway (asyncpg)
-if DB_URL.startswith("postgres://"):
-    DB_URL = DB_URL.replace("postgres://", "postgresql+asyncpg://", 1)
-
 if DB_URL.startswith("postgresql://"):
     DB_URL = DB_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
