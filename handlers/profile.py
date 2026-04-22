73d5d09ad6c0bdf3b62bf6069654c868c8a8f7e0
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-23 01:14:58 +0300

    Update profile.py

--- handlers/profile.py
+++ handlers/profile.py
@@ -1,1 +1,15 @@
+from aiogram import types
+from loader import dp
+from db.queries.users import get_or_create_user
 
+@dp.message_handler(text="профиль")
+async def profile(message: types.Message, db):
+    user = await get_or_create_user(db, message.from_user.id)
+
+    await message.answer(f"""
+👤 Профиль
+
+💰 {user.balance}
+🎴 {user.cards_count}
+🌳 {user.tree}
+""")
