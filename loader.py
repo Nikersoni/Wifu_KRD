94d171109954685a327dd2f542265eaab521bcff
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-23 01:10:07 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,1 +1,6 @@
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from config import TOKEN
 
+bot = Bot(token=TOKEN, parse_mode="HTML")
+dp = Dispatcher(bot, storage=MemoryStorage())
