4081cf6acfe3847627ef3a1329e21b66e23df1c4
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-23 01:10:42 +0300

    Update database.py

--- db/database.py
+++ db/database.py
@@ -1,1 +1,9 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from config import DB_URL
 
+engine = create_async_engine(DB_URL, echo=False)
+
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
