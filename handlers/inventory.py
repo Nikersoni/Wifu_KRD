1a2406affd3bb8f1bc9f41963a6074d1c544b0d5
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-23 01:14:29 +0300

    Update inventory.py

--- handlers/inventory.py
+++ handlers/inventory.py
@@ -1,1 +1,15 @@
+from aiogram import types
+from loader import dp
+from sqlalchemy import select
+from db.models import UserCard, Card
 
+@dp.message_handler(text="инвентарь")
+async def inv(message: types.Message, db):
+    result = await db.execute(
+        select(UserCard, Card)
+        .join(Card, UserCard.card_id == Card.id)
+        .where(UserCard.user_id == message.from_user.id)
+    )
+
+    rows = result.all()
+    await message.answer(f"🎒 Карточек: {len(rows)}")
