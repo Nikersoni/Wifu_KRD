f15d658e8499fbf87cecef594a92c42d7dff420f
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-23 02:11:03 +0300

    Update profile.py

--- handlers/profile.py
+++ handlers/profile.py
@@ -1,6 +1,6 @@
 from aiogram import types
 from loader import dp
 
-@dp.message_handler(lambda m: m.text and m.text.lower() == "профиль")
-async def profile_test(message: types.Message):
-    await message.answer("ПРОФИЛЬ РАБОТАЕТ ✅")
+@dp.message_handler()
+async def test_all(message: types.Message):
+    await message.answer("Я ЖИВОЙ")
